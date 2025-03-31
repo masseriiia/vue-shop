5cd85a7dd9eaa9bee21130a2a540c5c798010a03
Danil Sveshnikov <bloodshoww@mail.ru>
2025-03-31 15:14:34 +0300

    added header

---
+++ src/components/AppButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+ const props = defineProps<{
+   option: string
+ }>()
+</script>
+
+<template>
+  <button :class="[option]">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.base {
+  padding: 12px;
+  width: 120px;
+  height: 40px;
+  border-radius: 8px;
+  color: var(--ui-light-gray);
+  border: 1px solid var(--ui-accent);
+  background: var(--ui-accent);
+  transition: background 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.base:hover {
+  background: var(--ui-accent-active);
+}
+
+.base:focus {
+  border: 2px solid var(--ui-accent);
+  background: var(--ui-accent-active);
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+
+import AppButton from '@/components/AppButton.vue'
+</script>
+
+<template>
+  <header class="app-header">
+    <div class="container">
+      <div class="content">
+        <nav class="nav-section">
+          <h1 class="logo">Vue Shopping</h1>
+          <ul class="menu">
+            <li class="menu-item">
+              <RouterLink to="/">Товары</RouterLink>
+            </li>
+            <li class="menu-item">
+              <RouterLink to="/">
+                Магазины
+              </RouterLink>
+            </li>
+          </ul>
+        </nav>
+        <AppButton option="base">
+          Войти
+        </AppButton>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  .app-header {
+    position: relative;
+    padding-top: 26px;
+    padding-bottom: 26px;
+    background-color: var(--ui-white);
+  }
+
+.logo {
+    font-family: var(--second-family);
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 133%;
+    color: var(--ui-accent);
+  }
+
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .nav-section {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .menu {
+    display: flex;
+  }
+
+  .menu-item {
+    padding: 8px 16px 4px 16px;
+    width: 120px;
+    height: 40px;
+    font-family: var(--font-family);
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    text-align: center;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: color 0.3s ease-in-out;
+
+    a {
+      color: #757575;
+    }
+  }
+  .menu-item a:hover {
+    color: #595959;
+  }
+</style>
