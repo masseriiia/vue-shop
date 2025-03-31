e513dd712f9c642a54f6c13a93ef555104106661
Danil Sveshnikov <bloodshoww@mail.ru>
2025-03-31 20:41:42 +0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
-  <div>
+  <div class="app-container">
     <AppHeader/>
+    <main class="main">
+
+    </main>
+    <AppFooter/>
   </div>
 </template>
 
 <style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main {
+  flex: 1;
+}
 
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="app-footer">
+    <div class="container">
+      <div class="content">
+        <ul class="footer-links">
+          <li class="footer-link"><RouterLink to="/">Доставка и оплата</RouterLink></li>
+          <li class="footer-link"><RouterLink to="/">Оферта</RouterLink></li>
+          <li class="footer-link"><RouterLink to="/">О нас</RouterLink></li>
+        </ul>
+        <div class="social-icons">
+          <RouterLink to="/" class="social-icon whatsapp"><span class="visually-hidden"><img src="../assets/icons/whatsapp.svg"/></span></RouterLink>
+          <RouterLink to="/" class="social-icon telegram"><span class="visually-hidden"><img src="../assets/icons/telegram.svg"/></span></RouterLink>
+          <RouterLink to="/" class="social-icon instagram"><span class="visually-hidden"><img src="../assets/icons/instagram.svg"/></span></RouterLink>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.app-footer {
+  padding-top: 21px;
+  padding-bottom: 23px;
+  background: var(--ui-bluish);
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.footer-links {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+
+.footer-link {
+  font-family: var(--font-family);
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 100%;
+  a {
+    transition: color 0.3s ease-in-out;
+    color: var(--ui-gray);
+  }
+}
+
+.footer-link a:hover {
+  color: #101210;
+}
+
+.social-icons {
+  display: flex;
+  gap: 10px;
+}
+
+.visually-hidden {
+  height: 32px;
+  width: 32px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  background: var(--ui-gray);
+}
+</style>
